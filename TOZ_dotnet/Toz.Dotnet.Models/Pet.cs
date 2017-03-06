9fdc1ab47d4fcba237e51a1f3e57e3681e90eedc
dkleszczynski <danielkleszczynski@gmail.com>
2017-03-06 20:37:39 +0100

    Usuniete nadmiarowe pola prywatne. Dodane publiczne settery.

--- TOZ_dotnet/Toz.Dotnet.Models/Pet.cs
+++ TOZ_dotnet/Toz.Dotnet.Models/Pet.cs
@@ -9,55 +9,37 @@ namespace Toz.Dotnet.Models
 
     public class Pet
     {
-        private int _id;
-
-        private string _name;
-
-        private PetType _type;
-
-        private PetSex _sex;
-
-        private byte [] _photo;
-
-        private string _description;
-
-        private string _address;
-
-        private DateTime _addingTime;
-
-        private DateTime _lastEditTime;
-
-        //Properties:
-        public int Id {get {return _id;}}
-
-        public string Name {get {return _name;}}
-
-        public PetType Type {get {return _type;}}
-
-        public PetSex Sex {get {return _sex;}}
-
-        public byte [] Photo {get {return _photo;}}
-
-        public string Description {get {return _description;}}
-
-        public string Address {get {return _address;}}
-
-        public DateTime AddingTime {get {return _addingTime;}}
-
-        public DateTime LastEditTime { get {return _lastEditTime;}}
+        public int Id {get; set;}
+ 
+        public string Name {get; set;}
+ 
+        public PetType Type {get; set;}
+ 
+        public PetSex Sex {get; set;}
+ 
+        public byte [] Photo {get; set;}
+ 
+        public string Description {get; set;}
+ 
+        public string Address {get; set;}
+ 
+        public DateTime AddingTime  {get; set;}
+ 
+        public DateTime LastEditTime {get; set;}
 
         public Pet(int id, string name, PetType type, PetSex sex, byte [] photo, string description, string address,
                     DateTime addingTime, DateTime lastEditTime)
                 {
-                        _id = id;
-                        _name = name;
-                        _type = type;
-                        _sex = sex;
-                        _photo = photo;
-                        _description = description;
-                        _address = address;
-                        _addingTime = addingTime;
-                        _lastEditTime = lastEditTime;
+                    Id = id;
+                    Name = name;
+                    Type = type;
+                    Sex = sex;
+                    Photo = photo;
+                    Description = description;
+                    Address = address;
+                    AddingTime = addingTime;
+                    LastEditTime = lastEditTime;
+                        
                 }
     }
 }
