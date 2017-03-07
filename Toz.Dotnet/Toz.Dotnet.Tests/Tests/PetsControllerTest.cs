2d04fd92ce3a95dd3c61e831b9780ecbd739a752
Krystian Kaniewski <yonji94@gmail.com>
2017-03-07 13:52:57 +0100

    DI

--- Toz.Dotnet/Toz.Dotnet.Tests/Tests/PetsControllerTest.cs
+++ Toz.Dotnet/Toz.Dotnet.Tests/Tests/PetsControllerTest.cs
@@ -4,14 +4,22 @@ using Toz.Dotnet.Core.Services;
 using Toz.Dotnet.Models;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Toz.Dotnet.Core.Interfaces;
+using Toz.Dotnet.Tests.Helpers;
 
 namespace Toz.Dotnet.Tests.Tests {
     public class PetsControllerTest
     {
+        private IPetsManagementService _petsManagementService;
+        public PetsControllerTest()
+        {
+            _petsManagementService = ServiceProvider.Instance.Resolve<IPetsManagementService>();
+        }
+
         [Fact]
         public void Index_ReturnsAViewResult_WithAListOfPets()
         {
-            var controller = new PetsController(new PetsManagementService(new FilesManagementService()));
+            var controller = new PetsController(_petsManagementService);
 
             var result = controller.Index();
 
