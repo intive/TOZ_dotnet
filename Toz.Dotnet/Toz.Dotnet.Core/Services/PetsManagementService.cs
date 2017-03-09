4c0ee912576156214c7be2b1e84a9e41d0eef9bb
Łukasz Sielewicz <lsielewicz8@gmail.com>
2017-03-09 19:08:07 +0100

    Deleted unused method

--- Toz.Dotnet/Toz.Dotnet.Core/Services/PetsManagementService.cs
+++ Toz.Dotnet/Toz.Dotnet.Core/Services/PetsManagementService.cs
@@ -23,16 +23,6 @@ namespace Toz.Dotnet.Core.Services
             return _mockupPetsDatabase;
         }
 		
-        public List<Pet> GetSamplePets()
-        {
-            List<Pet> petsList = new List<Pet>(){
-                new Pet(0,"Tofik",PetType.Dog,PetSex.Female,new byte[0],"Przyjacielski piesek","Znaleziono na Bohaterow Warszawy",DateTime.Parse("2017-03-06 13:26"),DateTime.Parse("2017-03-07 13:55")),
-                new Pet(1,"Filemon",PetType.Cat,PetSex.Male,new byte[0],"Przyjacielski kotek","Znaleziono na Wydziale Informatyki ZUT",DateTime.Parse("2017-03-05 22:11"),DateTime.Parse("2017-03-07 12:55")),
-                new Pet(2,"Bonifacy",PetType.Cat,PetSex.Male,new byte[0],"Bystry kotek","Znaleziony w Parku Kasprowicza",DateTime.Parse("2017-03-07 10:11"),DateTime.Parse("2017-03-07 12:34"))
-            };
-            
-            return petsList;
-        }
 
         public bool UpdatePet(Pet pet)
         {
