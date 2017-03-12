b422280dfce2946907e73a732e84e74a740e4551
dkleszczynski <danielkleszczynski@gmail.com>
2017-03-12 16:05:36 +0100

    Added localized resource strings for validation error messages.

--- Toz.Dotnet/Toz.Dotnet.Models/Pet.cs
+++ Toz.Dotnet/Toz.Dotnet.Models/Pet.cs
@@ -1,19 +1,45 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using Toz.Dotnet.Models.EnumTypes;
+using Toz.Dotnet.Resources;
 
 namespace Toz.Dotnet.Models
 {
     public class Pet
     {
         public int Id {get; set;}
+
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resources.NewPetDataValidation),
+                  ErrorMessageResourceName = "EmptyField")]
+        [StringLength(30, ErrorMessageResourceType = typeof(Resources.NewPetDataValidation),
+                  ErrorMessageResourceName = "MaxLength")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessageResourceType = typeof(Resources.NewPetDataValidation),
+                  ErrorMessageResourceName = "NameLetters")]
         public string Name {get; set;}
+
+        [RegularExpression("^(Cat|Dog)$", ErrorMessageResourceType = typeof(Resources.NewPetDataValidation),
+                  ErrorMessageResourceName = "TypeUndefined")]
         public PetType Type {get; set;}
+
         public PetSex Sex {get; set;}
+
         public byte [] Photo {get; set;}
+
+        [StringLength(300, ErrorMessageResourceType = typeof(Resources.NewPetDataValidation),
+                  ErrorMessageResourceName = "MaxLength")]
         public string Description {get; set;}
+
+        [Required(AllowEmptyStrings = false, ErrorMessageResourceType = typeof(Resources.NewPetDataValidation),
+                  ErrorMessageResourceName = "EmptyField")]
+        [StringLength(100, ErrorMessageResourceType = typeof(Resources.NewPetDataValidation),
+                  ErrorMessageResourceName = "MaxLength")]
         public string Address {get; set;}
+
         public DateTime AddingTime  {get; set;}
+
         public DateTime LastEditTime {get; set;}
+
+        public Pet() { }
 
         public Pet(int id, string name, PetType type, PetSex sex, byte [] photo, string description, string address,
                     DateTime addingTime, DateTime lastEditTime)
@@ -27,8 +53,6 @@ namespace Toz.Dotnet.Models
                     Address = address;
                     AddingTime = addingTime;
                     LastEditTime = lastEditTime;
-                        
-                }
-        public Pet() {}
-    }
+                 }
+        }
 }
