88eccd953c7267df45042324319e605ccdcae4a5
Bartosz Hliwa <bartosz.hliwa@gmail.com>
2017-04-14 21:28:40 +0200

    Tests for news + Sebastian's tips

---
+++ Toz.Dotnet/Toz.Dotnet.Tests.Sanity/SanityTests.cs
@@ -0,0 +1,117 @@
+using Xunit;
+using Toz.Dotnet.Core.Interfaces;
+using Toz.Dotnet.Tests.Helpers;
+using Toz.Dotnet.Models;
+using Toz.Dotnet.Models.EnumTypes;
+using System;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Toz.Dotnet.Tests.Sanity
+{
+    public class SanityTests
+    {
+        private IPetsManagementService _petsManagementService;
+        private INewsManagementService _newsManagementService;
+        private Pet _testingPet;
+        private News _testingNews;
+        public SanityTests()
+        {
+            _petsManagementService = ServiceProvider.Instance.Resolve<IPetsManagementService>();
+            _newsManagementService = ServiceProvider.Instance.Resolve<INewsManagementService>();
+            
+            _testingPet = new Pet()
+            {
+                Id = null,
+                Name = "TestDog",
+                Type = PetType.DOG,
+                Sex = PetSex.MALE,
+                Photo = new byte[10],
+                Description = "Dog that eats tigers",
+                Address = "Found in the jungle",
+                AddingTime = DateTime.Now,
+                LastEditTime = DateTime.Now
+            };
+
+            _testingNews = new News()
+            {               
+                Id = System.Guid.NewGuid().ToString(),
+                Title = "TestNews",
+                PublishingTime = DateTime.Now,
+                AddingTime = DateTime.Now,
+                LastEditTime = DateTime.Now,
+                Body = "Text",
+                Photo = new byte[10],
+                Status = NewsStatus.RELEASED
+            };
+
+            _petsManagementService.RequestUri = RequestUriHelper.PetsUri;
+            _newsManagementService.RequestUri = RequestUriHelper.NewsUri;
+        }
+        
+
+        [Fact]
+        public void PetsFunctionalityTest()
+        {
+            Assert.True(!string.IsNullOrEmpty(_petsManagementService.RequestUri));
+            //Get all pets
+            var pets = _petsManagementService.GetAllPets().Result;
+            Assert.NotNull(pets);
+            //Get specified pet
+            var firstPet = pets.FirstOrDefault();
+            var pet = _petsManagementService.GetPet(firstPet.Id).Result;
+            Assert.NotNull(pet);
+            Assert.Null(_petsManagementService.GetPet("notExistingIDThatIsNotID--1").Result);
+            //Create pet
+            bool created = _petsManagementService.CreatePet(_testingPet).Result;
+            Assert.True(created);
+            if(created)
+            {
+                //Delete pet
+                Assert.True(_petsManagementService.DeletePet(_testingPet).Result);
+            }
+            //Update pet
+            string petName = firstPet.Name;
+            firstPet.Name = "SanityTestName";
+            bool updated = _petsManagementService.UpdatePet(firstPet).Result;
+            Assert.True(updated);
+            if(updated)
+            {
+                firstPet.Name = petName;
+                Assert.True(_petsManagementService.UpdatePet(firstPet).Result);
+            }
+        }
+
+        [Fact]
+        public void NewsFunctionalityTest()
+        {
+            Assert.True(!string.IsNullOrEmpty(_newsManagementService.RequestUri));
+            //Get all news
+            var news = _newsManagementService.GetAllNews().Result;
+            Assert.NotNull(news);
+            //Get specified news
+            var firstNews = news.FirstOrDefault();
+            var singleNews = _newsManagementService.GetNews(firstNews.Id).Result;
+            Assert.NotNull(singleNews);
+            Assert.Null(_newsManagementService.GetNews("notExistingIDThatIsNotID--1").Result);
+            //Create news
+            bool created = _newsManagementService.CreateNews(_testingNews).Result;
+            Assert.True(created);
+            if(created)
+            {
+                //Delete news
+                Assert.True(_newsManagementService.DeleteNews(_testingNews).Result);
+            }
+            //Update news
+            string newsTitle = firstNews.Title;
+            firstNews.Title = "SanityTestName";
+            bool updated = _newsManagementService.UpdateNews(firstNews).Result;
+            Assert.True(updated);
+            if(updated)
+            {
+                firstNews.Title = newsTitle;
+                Assert.True(_newsManagementService.UpdateNews(firstNews).Result);
+            }
+        }
+    }
+}
