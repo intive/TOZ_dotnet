d28e191887a4cbe8a0cde3145e187ab1ce39759a
dkleszczynski <danielkleszczynski@gmail.com>
2017-04-21 22:16:09 +0200

    Organization Model.

---
+++ Toz.Dotnet/Toz.Dotnet.Models/OrganizationInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Toz.Dotnet.Models.JsonConventers;
+using Toz.Dotnet.Models.OrganizationSubtypes;
+
+namespace Toz.Dotnet.Models
+{
+    public class OrganizationInfo
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+                
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+
+        [JsonProperty("contact")]
+        public Contact Contact { get; set; }
+
+        [JsonProperty("bankAccount")]
+        public BankAccount BankAccount { get; set; }
+    }
+}
---
+++ Toz.Dotnet/Toz.Dotnet.Models/OrganizationSubtypes/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Toz.Dotnet.Models.JsonConventers;
+using Toz.Dotnet.Resources.CustomValidationAttributes;
+
+namespace Toz.Dotnet.Models.OrganizationSubtypes
+{
+    public class Contact
+    {
+        [JsonProperty("email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [JsonProperty("phone")]
+        [PhoneNumber]
+        public string Phone { get; set; }
+
+        [JsonProperty("fax")]
+        [RegularExpression("^[0-9]*$", ErrorMessageResourceType = typeof(Resources.ModelsDataValidation), ErrorMessageResourceName = "OnlyDigits")]
+        public string Fax { get; set; }
+
+        [JsonProperty("website")]
+        public string Website { get; set; }
+    }
+}
