6ad27ce0c976061e03170191c3b4ef8487014a4b
Astralius <adrian.standowicz@gmail.com>
2017-04-27 00:34:58 +0200

    [Feature]: added view for Organization Info with both read-only and edit mode support. [Feature]: added basic controller implementation. [Feature]: added IOrganizationManagementService interface for the future service. [Feature]: added PL localization strings for the above. [Feature]: added backend urls for OrganizationInfo API [Feature]: added "Organization Info" button to _Layout.cshtml (top bar) [Feature]: added OnlyDigits localization strings to Resources/ModelsDataValidation Designer and resource files. [Bug fix]: Add.cshtml -> changed Localizer["Title"] to Localizer["PageTitle"] to match resources string [Refactoring]: renamed OrganizationInfo class name & file name to Organization to match backend naming [Refactoring]: removed unnecessary usings from Organization.cs [Refactoring]: changed *Ładne niemieckie zdanie* to "Wählen Sie das Geschlecht des Tiers aus." in ModelsDataValidation.de.resx -> PetSexUndefined ;-)

---
+++ Toz.Dotnet/Toz.Dotnet/Controllers/OrganizationController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Toz.Dotnet.Core.Interfaces;
+using Toz.Dotnet.Models;
+using Toz.Dotnet.Resources.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Localization;
+using System.Threading.Tasks;
+using System.Threading;
+using Toz.Dotnet.Models.OrganizationSubtypes;
+using System;
+
+namespace Toz.Dotnet.Controllers
+{
+    public class OrganizationController : Controller
+    {
+        private IOrganizationManagementService _organizationManagementService;
+        private readonly IStringLocalizer<OrganizationController> _localizer;
+        private readonly AppSettings _appSettings;
+
+        //public OrganizationController(IOrganizationManagementService organizationManagementService, IStringLocalizer<OrganizationController> localizer, IOptions<AppSettings> appSettings)
+        //{
+        //    _organizationManagementService = organizationManagementService;
+        //    _localizer = localizer;
+        //    _appSettings = appSettings.Value;
+        //}
+
+        public OrganizationController(IStringLocalizer<OrganizationController> localizer, IOptions<AppSettings> appSettings)
+        {
+            _localizer = localizer;
+            _appSettings = appSettings.Value;
+        }
+
+        public IActionResult Info(bool edit = false)
+        {
+            ViewData["EditMode"] = edit;
+
+            //var organization = await _organizationManagementService.GetOrganizationInfo();
+            Address a = new Address
+            {
+                Street = "Testowa",
+                HouseNumber = "25",
+                ApartmentNumber = "10",
+                PostCode = "71-353",
+                City = "Szczecin",
+                Country = "Polska"
+            };
+
+            Contact c = new Contact
+            {
+                Email = "toz@intive.com",
+                Phone = "123456789",
+                Fax = "123456789",
+                Website = "http://www.toz-szczecin.pl"
+            };
+
+            BankAccount b = new BankAccount
+            {
+                Number = "12345678901234567890123456",
+                BankName = "Bank Opieki nad Zwierzętami SA"
+            };
+
+            Organization organization = new Organization
+            {
+                Name = "Towarzystwo Opieki nad Zwięrzętami",
+                Address = a,
+                Contact = c,
+                BankAccount = b
+            };
+            return View(organization);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Organization organization, CancellationToken cancellationToken)
+        {
+            if (organization != null && ModelState.IsValid)
+            {
+                if (await _organizationManagementService.CreateOrganizationInfo(organization, cancellationToken))
+                {
+                    return RedirectToAction("Info");
+                }
+                return BadRequest();
+            }
+            return View(organization);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Organization organization, CancellationToken cancellationToken)
+        {
+            if (organization != null && ModelState.IsValid)
+            {
+                if (await _organizationManagementService.UpdateOrganizationInfo(organization, cancellationToken))
+                {
+                    return RedirectToAction("Info", new { edit = false });
+                }
+                return BadRequest();
+            }
+            return View(organization);
+        }
+    }
+}
