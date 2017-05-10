2e15847d5354851382073a2545cab4b5602463ed
Krystian Kaniewski <yonji94@gmail.com>
2017-05-11 00:08:48 +0200

    Fixed client side validation & proper modal closing

--- Toz.Dotnet/Toz.Dotnet/wwwroot/js/site.js
+++ Toz.Dotnet/Toz.Dotnet/wwwroot/js/site.js
@@ -1,13 +1,5 @@
 ﻿// Write your Javascript code.
 $(document).ready(function () {
-    $('input[type="submit"]').on('click', function () {
-        $(this).prop("disabled", true);
-    });
-
-    $('input[type="button"]').on('click', function () {
-        $(this).prop("disabled", true);
-    });
-
     $(document).on('change', '.btn-file :file', function () {
         var input = $(this),
             label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
@@ -26,6 +18,7 @@ $(document).ready(function () {
         }
 
     });
+
     function readURL(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
@@ -44,7 +37,9 @@ $(document).ready(function () {
 
     $(function () {
 
-        $.ajaxSetup({ cache: false });
+        $.ajaxSetup({
+            cache: false
+        });
 
         $("a[data-modal]").on("click", function (e) {
 
@@ -53,7 +48,7 @@ $(document).ready(function () {
 
 
             $('#myModalContent').load(this.href, function () {
-
+                $.validator.unobtrusive.parse(this);
 
                 $('#myModal').modal({
                     /*backdrop: 'static',*/
@@ -109,4 +104,38 @@ $(document).ready(function () {
     $('#search').keyup(function () {
         $('#table').DataTable().search($(this).val()).draw();
     })
+
+    jQuery.validator.setDefaults({
+        highlight: function (element, errorClass, validClass) {
+            if (element.type === 'radio') {
+                this.findByName(element.name).addClass(errorClass).removeClass(validClass);
+            } else {
+                $(element).addClass(errorClass).removeClass(validClass);
+                $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+                $(element).closest('.form-control-feedback').removeClass('glyphicon-ok').addClass('glyphicon-remove');
+                $(element).nextAll('.glyphicon').removeClass('hidden');
+                $(element).nextAll('.glyphicon').removeClass('glyphicon-ok').addClass('glyphicon-remove');
+                $(element).nextAll('.glyphicon').removeClass('has-success').addClass('has-error');
+            }
+        },
+        unhighlight: function (element, errorClass, validClass) {
+            if (element.type === 'radio') {
+                this.findByName(element.name).removeClass(errorClass).addClass(validClass);
+            } else {
+                $(element).removeClass(errorClass).addClass(validClass);
+                $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+                $(element).nextAll('.glyphicon').removeClass('hidden');
+                $(element).nextAll('.glyphicon').removeClass('glyphicon-remove').addClass('glyphicon-ok');
+                $(element).nextAll('.glyphicon').removeClass('has-error').addClass('has-success');
+            }
+        }
+    });
+
+    $(function () {
+
+        $("span.field-validation-valid, span.field-validation-error").addClass('help-block');
+        $("div.form-group").has("span.field-validation-error").addClass('has-error');
+        $("div.validation-summary-errors").has("li:visible").addClass("alert");
+
+    });
 });
