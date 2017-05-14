6c7cd39cb4c03fbf2a106f6487c4353abd2a148c
Łukasz Sielewicz <lsielewicz8@gmail.com>
2017-05-14 17:38:46 +0200

    build fix

--- Toz.Dotnet/Toz.Dotnet.Models/User.cs
+++ Toz.Dotnet/Toz.Dotnet.Models/User.cs
@@ -2,8 +2,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using Toz.Dotnet.Models.CustomValidationAttributes;
 using Toz.Dotnet.Models.EnumTypes;
+using Toz.Dotnet.Models.Validation;
 
 namespace Toz.Dotnet.Models
 {
