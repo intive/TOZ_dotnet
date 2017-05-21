1d740ae7903b122395c8a93a2970777e30b4bf6e
Łukasz Sielewicz <lsielewicz8@gmail.com>
2017-05-21 13:07:59 +0200

    Model and service

---
+++ Toz.Dotnet/Toz.Dotnet.Core/Services/HowToHelpInformationService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Toz.Dotnet.Core.Interfaces;
+using Toz.Dotnet.Models;
+using Toz.Dotnet.Models.EnumTypes;
+using Toz.Dotnet.Resources.Configuration;
+
+namespace Toz.Dotnet.Core.Services
+{
+    public class HowToHelpInformationService : IHowToHelpInformationService
+    {
+        private readonly IRestService _restService;
+
+        public string BecomeVolunteerUrl { get; set; }
+        public string DonateInfoUrl { get; set; }
+
+        public HowToHelpInformationService(IRestService restService, IOptions<AppSettings> appSettings)
+        {
+            _restService = restService;
+            BecomeVolunteerUrl = appSettings.Value.BackendBecomeVolunteerUrl;
+            DonateInfoUrl = appSettings.Value.BackendDonateInfoUrl;
+        }
+
+        public async Task<bool> UpdateOrCreateHelpInfo(HowToHelpInfo helpInfo, HowToHelpInfoType type, CancellationToken cancelationToken = new CancellationToken())
+        {
+            string currentUrl;
+            if (!GetCurrentUrl(type, out currentUrl))
+            {
+                return false;
+            }
+
+            Func<string, HowToHelpInfo, CancellationToken, Task<bool>> methodToExecute = _restService.ExecutePutAction;
+
+            if (await GetHelpInfo(type, cancelationToken) == null)
+            {
+                methodToExecute = _restService.ExecutePostAction;
+            }
+
+            return await methodToExecute(currentUrl, helpInfo, cancelationToken);
+        }
+
+        public async Task<HowToHelpInfo> GetHelpInfo(HowToHelpInfoType type, CancellationToken cancellationToken = new CancellationToken())
+        {
+            string currentUrl;
+            if (!GetCurrentUrl(type, out currentUrl))
+            {
+                return null;
+            }
+
+            return await _restService.ExecuteGetAction<HowToHelpInfo>(currentUrl, cancellationToken);
+        }
+
+        private bool GetCurrentUrl(HowToHelpInfoType type, out string resultUrl)
+        {
+            switch (type)
+            {
+                case HowToHelpInfoType.BecomeVolunteer:
+                    resultUrl = BecomeVolunteerUrl;
+                    break;
+                case HowToHelpInfoType.Donate:
+                    resultUrl = BecomeVolunteerUrl;
+                    break;
+                default:
+                    resultUrl = string.Empty;
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Toz.Dotnet/Toz.Dotnet.Models/HowToHelpInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Newtonsoft.Json;
+using Toz.Dotnet.Models.JsonConventers;
+
+namespace Toz.Dotnet.Models
+{
+    public class HowToHelpInfo
+    {
+        [JsonProperty("modificationDate")]
+        [JsonConverter(typeof(JsonDateTimeConventer))]
+        public DateTime? CreationTime { get; set; }
+
+        [JsonProperty("howToHelpDescription")]
+        public string Description { get; set; }
+
+
+    }
+}
