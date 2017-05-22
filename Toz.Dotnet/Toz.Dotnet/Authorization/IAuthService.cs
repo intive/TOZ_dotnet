698503f311206489359c4be4e5c591dbf075c831
MatKumpf <mateusz.kumpf@gmail.com>
2017-05-23 00:26:17 +0200

    Authorize Middleware and AuthService

---
+++ Toz.Dotnet/Toz.Dotnet/Authorization/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Toz.Dotnet.Authorization
+{
+    public interface IAuthService
+    {
+        void AddToCookie(HttpContext httpContext, string key, string value, CookieOptions cookieOptions);
+        string ReadCookie(HttpContext httpContext, string key);
+        void RemoveCookie(HttpContext httpContext, string key);
+
+        bool IsAuth { get; }
+        void SetIsAuth(bool value);
+    }
+}
