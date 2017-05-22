4398498a7b47c27f18d5cb7720f18607cb6ffd6a
MatKumpf <mateusz.kumpf@gmail.com>
2017-05-23 00:34:29 +0200

    Add "token" parameter

--- Toz.Dotnet/Toz.Dotnet.Core/Services/FilesManagementService.cs
+++ Toz.Dotnet/Toz.Dotnet.Core/Services/FilesManagementService.cs
@@ -9,14 +9,14 @@ namespace Toz.Dotnet.Core.Services
 {
     public class FilesManagementService : IFilesManagementService
     {
-        public Image DownloadImage(string address)
+        public Image DownloadImage(string address, string token)
         {
             HttpClient httpClient = new HttpClient();
             var imgBytes = httpClient.GetStreamAsync(address);
             return Image.FromStream(imgBytes.Result);
         }
 
-        public bool UploadImage(Image image)
+        public bool UploadImage(Image image, string token)
         {
             //todo: use a magic craft to send image to the backend.
             return true;
