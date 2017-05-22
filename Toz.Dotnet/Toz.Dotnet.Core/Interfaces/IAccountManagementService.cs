67a34a7869a8c0335a33a9941e29bed34dfa2909
MatKumpf <mateusz.kumpf@gmail.com>
2017-05-23 00:36:55 +0200

    Add AccountManagementService and remove old auth method

---
+++ Toz.Dotnet/Toz.Dotnet.Core/Interfaces/IAccountManagementService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Toz.Dotnet.Models;
+
+namespace Toz.Dotnet.Core.Interfaces
+{
+    public interface IAccountManagementService
+    {
+        Task<JwtToken> LogIn(Login login, CancellationToken cancelationToken = default(CancellationToken));
+    }
+}
