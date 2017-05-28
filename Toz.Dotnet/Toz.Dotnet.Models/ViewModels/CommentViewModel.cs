bc3bf0efcc5772f6400ecfec086a87704d9f142c
dkleszczynski <danielkleszczynski@gmail.com>
2017-05-29 01:13:59 +0200

    CommentViewModel

---
+++ Toz.Dotnet/Toz.Dotnet.Models/ViewModels/CommentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Toz.Dotnet.Models.ViewModels
+{
+    public class CommentViewModel
+    {
+        public Comment TheComment { get; set; }
+        public User TheUser { get; set; }
+
+        public DateTime Created => TheComment.Created;
+        public string UserIdentity => $"{TheUser.FirstName} {TheUser.LastName}";
+        public string Contents => TheComment.Contents;
+        
+    }
+}
--- Toz.Dotnet/Toz.Dotnet/Controllers/CommentsController.cs
+++ Toz.Dotnet/Toz.Dotnet/Controllers/CommentsController.cs
@@ -10,23 +10,34 @@ using Toz.Dotnet.Resources.Configuration;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using Toz.Dotnet.Models;
+using Toz.Dotnet.Models.ViewModels;
 
 namespace Toz.Dotnet.Controllers
 {
     public class CommentsController : TozControllerBase<CommentsController>
     {
         private readonly ICommentsManagementService _commentsManagementService;
+        private readonly IUsersManagementService _usersManagementService;
 
-        public CommentsController(ICommentsManagementService commentsManagementService,
+        public CommentsController(ICommentsManagementService commentsManagementService, IUsersManagementService usersManagementService,
             IBackendErrorsService backendErrorsService, IStringLocalizer<CommentsController> localizer, IOptions<AppSettings> settings, IAuthService authService) : base(backendErrorsService, localizer, settings, authService)
         {
             _commentsManagementService = commentsManagementService;
+            _usersManagementService = usersManagementService;
         }
 
         public async Task<IActionResult> Index(CancellationToken cancellationToken)
         {
             List<Comment> comments = await _commentsManagementService.GetAllComments(AuthService.ReadCookie(HttpContext, AppSettings.CookieTokenName, true), cancellationToken);
-            return View(comments.OrderByDescending(x => x.Created).ThenByDescending(x => x.LastModified).ToList());
+            List<CommentViewModel> viewModels = new List<CommentViewModel>();
+
+            foreach(Comment comment in comments)
+            {
+                User user = await _usersManagementService.GetUser(comment.UserUuid, AuthService.ReadCookie(HttpContext, AppSettings.CookieTokenName, true));
+                viewModels.Add(new CommentViewModel() { TheUser = user, TheComment = comment });
+            }
+
+            return View(viewModels.OrderByDescending(x => x.Created).ToList());
         }
     }
 }
