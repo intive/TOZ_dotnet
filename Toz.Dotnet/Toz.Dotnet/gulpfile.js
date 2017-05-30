7cf6d1a20d6b08963f53d9a51608146262abcf75
Astralius <adrian.standowicz@gmail.com>
2017-05-30 10:35:10 +0200

    [Hot-Fix]: 1. Changed all libs to static in _Layout.cshtml 2. Redirected all bower dependencies to wwwroot/bower-libs folder 3. Added bower-libs folder to .gitignore 4. Modified all gulp tasks to ignore bower-libs folder during minification & less/sass compilation 5. Removed all project data from the static libs, leaving behind distribution files only

--- Toz.Dotnet/Toz.Dotnet/gulpfile.js
+++ Toz.Dotnet/Toz.Dotnet/gulpfile.js
@@ -13,31 +13,30 @@ var gulp = require('gulp'),
     gutil = require('gulp-util');
 
 gulp.task("less", function () {
-    return gulp.src("wwwroot/**/*.less", { base: "./" })
+    return gulp.src(["!wwwroot/bower-libs/**", "wwwroot/**/*.less"], { base: "./" })
         .pipe(less())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("sass", function () {
-    return gulp.src("wwwroot/**/*.scss", { base: "./" })
+    return gulp.src(["!wwwroot/bower-libs/**", "wwwroot/**/*.scss"], { base: "./" })
         .pipe(sass())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("minify-js", function () {
-    return gulp.src(['wwwroot/**/*.js', '!wwwroot/**/*.min.js'], { base: "./" })
+    return gulp.src(["!wwwroot/bower-libs/**", "wwwroot/**/*.js", "!wwwroot/**/*.min.js"], { base: "./" })
         .pipe(minify({
                 ext: {
                     min: '.min.js'
-                },
-                exclude: ['test']
+                }
             })
             .on('error', gutil.log))
         .pipe(gulp.dest("."));
 });
 
 gulp.task("minify-css", function () {
-    return gulp.src(['wwwroot/**/*.css', '!wwwroot/**/*.min.css'], { base: "./" })
+    return gulp.src(['!wwwroot/bower-libs/**', 'wwwroot/**/*.css', '!wwwroot/**/*.min.css'], { base: "./" })
         .pipe(cssmin())
         .pipe(rename({ suffix: ".min" }))
         .pipe(gulp.dest("."));
