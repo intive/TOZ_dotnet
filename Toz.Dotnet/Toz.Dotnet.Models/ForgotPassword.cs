d81b5019168a5ece2e17403c2fdfb45302d4379d
MatKumpf <mateusz.kumpf@gmail.com>
2017-06-18 22:04:41 +0200

    Add ForgotPassword model

---
+++ Toz.Dotnet/Toz.Dotnet.Models/ForgotPassword.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Toz.Dotnet.Models
+{
+    public class ForgotPassword
+    {
+        [Required(ErrorMessageResourceType = typeof(Resources.ModelsDataValidation), ErrorMessageResourceName = "EmptyField")]
+        [JsonProperty("email")]
+        [EmailAddress(ErrorMessageResourceType = typeof(Resources.ModelsDataValidation), ErrorMessageResourceName = "EmailValidationMessage")]
+        public string Email { get; set; }
+    }
+}
